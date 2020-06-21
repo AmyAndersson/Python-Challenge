1a271ffd8a102bea618ea56321e694db77d01655
AmyAndersson <66049569+AmyAndersson@users.noreply.github.com>
2020-06-21 20:27:29 +0800

    starting main.py

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -1,1 +1,19 @@
-print(testing)
+import os
+import csv
+
+#create path
+csvpath = os.path.join("..","Resources", "pyBanks_budget_data.csv")
+
+#open and read csv file
+with open(csvpath) as csvfile:
+   
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+    print (f"CSV header: {csv_header}")
+
+    """ for row in csvreader:
+        print(row) """
+    sum(1 for line in csvreader)
+    #print("Total months" str(sum(1 for line in csvreader)))
+    sum
+
