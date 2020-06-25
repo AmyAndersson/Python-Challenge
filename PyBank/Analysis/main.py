e7a53277220f813c47a0259df8995d664a1ab0e3
AmyAndersson <66049569+AmyAndersson@users.noreply.github.com>
2020-06-25 18:06:47 +0800

    FINISHED

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -4,58 +4,62 @@ import csv
 
 budgetpath = os.path.join("..","Resources", "PyBanks_budget_data.csv")
     
-
-    #1. Count the lines within the csv
-
-    #2. sum the entire profit_loss column to find total.
-
-    #3. activate writer and add "change" column in csv
-
-    #take row 2 from row 1 to determine "change"
-
-    #find average of new change column to create a "average change"
-
-    #open and read csv file
 with open(budgetpath) as csvfile:
       
-        #1  
+       #csv reader  
        Budget_csv = csv.reader(csvfile, delimiter=",")
        header = next(Budget_csv)
-       #print (f"CSV header: {header}")
+
+       #defining variables
        row_count = 0
        Total = 0
        TotalChange = 0
        PreviousTotal = 0
        MaxChange = 0
        MinChange = 0
-
+       
+       #create a for loop that runs through the dataset
        for row in Budget_csv:
+           #determine the total sum of profits and loss
             Total = Total + int(row[1])
+            #create an if statement for change (row 2 - row 1)
             if row_count == 0:
                 change = int(row[1])
-                PreviousTotal = change
+                PreviousTotal = change   
             else:
                 change = int(row[1]) - PreviousTotal
                 TotalChange = TotalChange + change
                 PreviousTotal = int(row[1])
+                #create an if statement to replace maxchange everytime a bigger value is found. Do same for min
                 if change > MaxChange:
                     MaxChange = change
                     MaxDate = row[0]
                 if change < MinChange:
                     MinChange = change
                     MinDate = row[0]
-
-
-
-        
+            #add 1 to row to count data points
             row_count = row_count + 1
+#     #find the average change             
+averageChange = (round(TotalChange / (row_count - 1),2))
 
-#totalchange = sum(change)
-averageChange = TotalChange / (row_count - 1)
-
+print("Financial Analysis")
+print("-----------------------")
 print(f"Total Months: {row_count}")
 print(f"Total:{Total}")
-print(f"Average Change: {averageChange}")
-print(f"Greatest increase in Profits on {MaxDate}: {MaxChange}")
-print(f"Greatest decrease in Profits on {MinDate}: {MinChange}")
-    
+print(f"Average Change: ${averageChange}")
+print(f"Greatest increase in Profits on {MaxDate}: (${MaxChange})")
+print(f"Greatest decrease in Profits on {MinDate}: (${MinChange})")
+   
+
+output_path = os.path.join("..", "Results", "Financial Analysis.csv")
+
+with open(output_path, 'w', newline="") as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow("Financial Analysis")
+    csvwriter.writerow("-----------------------")
+    csvwriter.writerow(f"Total:{Total}")
+    csvwriter.writerow(f"Average Change: ${averageChange}")
+    csvwriter.writerow(f"Greatest increase in Profits on {MaxDate}: (${MaxChange})")
+    csvwriter.writerow(f"Greatest decrease in Profits on {MinDate}: (${MinChange})")
+
+   
